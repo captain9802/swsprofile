d8e8946cd49bf67b9c7c3be021d23f6e98b20fe4
SonWuSeung <captain9802@gmail.com>
2025-02-20 17:49:41 +0900

    Add : - 레이아웃 설정 - sass install - 내 자기소개 컴포넌트 추가

---
+++ src/components/mypage/MyIntroduction.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="introduction">
+    <div class="introduction-info">
+      <div class="introduction-header">
+        <div class="image-container">
+          <img src="/sws.jpg" alt="프로필 이미지" class="profile-image"/>
+        </div>
+        <p>손우성</p>
+      </div>
+      <div class="introduction-content">
+        <p>
+          🔗 GitHub: captain9802<br><br>
+          👤 이름: 손우성<br><br>
+          📅 생년월일: 1998.02.24<br><br>
+          📍 위치: 경기도 군포시<br><br>
+          📞 연락처: 010-9493-9521<br><br>
+          📧 이메일: captain9802@gmail.com
+        </p>
+      </div>
+    </div>
+    <div class="introduction-myself">
+      <p class="introduction-hi">
+        안녕하세요! 👋
+        도전과 해결을 즐기는 개발자 손우성입니다.<br><br>
+        React와 Vue를 활용하여 더 나은 웹 경험을 만드는 것을 목표로 하고 있으며, 디자인 툴인 Figma를 통해 사용자 친화적인 UI를 구현하는 데 집중하고 있습니다.<br><br>
+
+        새로운 기술을 배우는 것을 즐기며, 문제를 해결하는 과정에서 성장하는 것이 개발의 가장 큰 매력이라고 생각합니다.
+        더 좋은 서비스를 만들기 위해 끊임없이 고민하고 도전합니다.<br><br>
+        궁금한 점이 있거나, 함께 이야기 나누고 싶다면 언제든 연락 주세요!
+        📧 captain9802@gmail.com
+        감사합니다! 😊
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyIntroduction"
+}
+</script>
+
+<style lang="scss">
+.introduction {
+  background-color: #fff;
+  border: 1px solid #ddd;
+  justify-items: center;
+  height: 100%;
+  align-content: center;
+  display: grid;
+  gap: 5rem;
+}
+
+.introduction-header {
+  justify-items: center;
+  align-content: center;
+}
+
+.introduction-info {
+  display: flex;
+  gap: 7rem;
+}
+
+.introduction-hi {
+  max-width: 700px;
+}
+
+.image-container {
+  width: 140px;
+  height: 140px;
+  overflow: hidden;
+  border-radius: 50%;
+}
+
+.profile-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
---
+++ src/main/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="layout">
+    <div class="left">
+     <MyIntroduction/>
+    </div>
+    <div class="tab">
+      <p>탭</p>
+    </div>
+    <div class="project">
+      <p>프로젝트 소개</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyIntroduction from "@/components/mypage/MyIntroduction.vue";
+
+export default {
+  name: "MyPage",
+  components: {MyIntroduction}
+}
+</script>
+
+<style lang="scss" scoped>
+body {
+  margin: 0;
+}
+.layout {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+
+.left {
+  width: 878px;
+  background-color: #f4f4f4;
+  padding: 20px;
+}
+
+.tab {
+  width: 80px;
+  background-color: #ddd;
+}
+
+.project {
+  flex-grow: 1;
+  background-color: #eaeaea;
+  padding: 20px;
+}
+</style>
